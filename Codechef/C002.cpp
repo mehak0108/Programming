e9c35cdc4a1119d134a7d3f222f5e84cc35c2fee
mehak0108 <mehakmittal63@yahoo.in>
2018-07-06 14:44:53 +0530

    Corrected

--- Codechef/C002.cpp
+++ Codechef/C002.cpp
@@ -2,26 +2,40 @@
 using namespace std;
 
 int main(){
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     int t;
     cin>>t;
-    string s,b,e="";
+    
     while(t--){
+        string s,b,e="";
+        vector<string> v;
         int n;
         cin>>n;
         set<string> pass;
-        for(int i=1;i<=n;i++){
+        for(int i=0;i<n;i++){
             cin>>s;
             pass.insert(s);
         }
         cin>>b;
-        for(int i=0;i<b.length();b++){
-            if(pass.find(e)==pass.end()){
-                e.push_back(b[i]);
-            }
-            else{
-                e.erase();
-                
+        for(int i=0;i<b.length();i++){
+            e+=b[i];
+            if(pass.find(e)!=pass.end()){
+                v.push_back(e);
+                e = "";
             }
         }
+        int k=0;
+        for(int i=0;i<v.size();i++)
+            k+=v[i].size();
+        if(v.size()==0 || k!=b.length()){
+            cout<<"WRONG PASSWORD\n";
+        }
+        else{
+            for(int i=0;i<v.size();i++)
+                cout<<v[i]<<" ";
+            cout<<"\n";
+        }
+        
     }
+    return 0;
 }
